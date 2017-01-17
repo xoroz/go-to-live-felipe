1e7608f9dd34ad9746972d65e3a80ac1085436de
Felipe Couto <fel.h2o@gmail.com>
2017-01-17 16:53:04 +0100

    Update restartserver.sh
    add send email cmd

--- scripts/restartserver.sh
+++ scripts/restartserver.sh
@@ -7,3 +7,5 @@ echo "Re-Starting lighttpd" >> $LOG
 service lighttpd restart >> $LOG
 aws deploy list-deployments --application-name codedeploy_github --deployment-group-name test  |grep "d-"|sed 's/,//g' |head -n1 |xargs aws deploy batch-get-deployments --deployment-ids $1 >> $LOG
 chown lighttpd. $LOG
+echo "Auto Deploy from github project.felipeferreira.net" |/bin/mail -s "AutoDeploy from github" -a /var/www/lighttpd/project/log/deploy-cmd.log -r "felipe@mail.felipeferreira.net"  fel.h2o@gmail.com
+exit 0
