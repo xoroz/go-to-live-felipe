820e9ec5cf7bcd874c3c9fa75e05a0ffd2b59281
Felipe Couto <fel.h2o@gmail.com>
2017-01-17 15:52:52 +0100

    Update restartserver.sh

--- scripts/restartserver.sh
+++ scripts/restartserver.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-echo "Re-Starting lighttpd" >> /var/log/cloud-init-cmd.log 2>&1
-date >> /var/log/cloud-init-cmd.log 2>&1 
-service lighttpd restart >> /var/log/cloud-init-cmd.log 2>&1
+# Felipe Ferreira Jan/2017
+LOG="/var/www/lighttpd/project/log/deploy-cmd.log"
+echo -e "------------------\n\n\n" >> $LOG
+date >> $LOG
+echo "Re-Starting lighttpd" >> $LOG
+service lighttpd restart >> $LOG
+aws deploy list-deployments --application-name codedeploy_github --deployment-group-name test  |grep "d-"|sed 's/,//g' |head -n1 |xargs aws deploy batch-get-deployments --deployment-ids $1 >> $LOG
+chown lighttpd. $LOG
