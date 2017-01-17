df08ab01710858d7aef7e89ea73b0acd0bf8bda9
Felipe Couto <fel.h2o@gmail.com>
2017-01-17 15:26:51 +0100

    Update beforeinstall.sh

--- scripts/beforeinstall.sh
+++ scripts/beforeinstall.sh
@@ -1,3 +1,6 @@
 if [ -d /var/www/lighttpd/project/ ]; then
     rm -rf /var/www/lighttpd/project/*
 fi
+if [ -f /etc/lighttpd/vhosts.d/project.felipeferreira.net.conf ]
+ rm -f /etc/lighttpd/vhosts.d/project.felipeferreira.net.conf
+fi
